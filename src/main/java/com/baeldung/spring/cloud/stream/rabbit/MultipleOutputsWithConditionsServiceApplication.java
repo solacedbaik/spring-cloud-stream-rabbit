da8aef8f552a9d6e63b769c8eacb683f3a777e0b
solacedbaik <dale.baik@solace.com>
2018-07-16 12:37:27 -0400

    Initial commit

---
+++ src/main/java/com/baeldung/spring/cloud/stream/rabbit/MultipleOutputsWithConditionsServiceApplication.java
@@ -0,0 +1,40 @@
+package com.baeldung.spring.cloud.stream.rabbit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.Message;
+
+import org.springframework.messaging.support.MessageBuilder;
+
+import com.baeldung.spring.cloud.stream.rabbit.processor.ConditionalProcessor;
+
+@SpringBootApplication
+@EnableBinding(ConditionalProcessor.class)
+public class MultipleOutputsWithConditionsServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleOutputsWithConditionsServiceApplication.class, args);
+    }
+
+    @Autowired
+    private ConditionalProcessor processor;
+
+    @StreamListener(target = ConditionalProcessor.INPUT, condition = "headers['type']=='bogey'")
+    public void routeValuesToAnOutput(Integer val) {
+    	processor.highOutput().send(message("bogey"));
+        processor.lowOutput().send(message(val));
+    }
+
+    @StreamListener(target = ConditionalProcessor.INPUT, condition = "headers['type']=='eagle'")
+    public void routeValuesToAnotherOutput(Integer val) {
+    	processor.highOutput().send(message("eagle"));
+        processor.highOutput().send(message(val));
+    }
+
+    private static final <T> Message<T> message(T val) {
+        return MessageBuilder.withPayload(val)
+            .build();
+    }
+}
---
+++ src/main/java/com/baeldung/spring/cloud/stream/rabbit/RabbitController.java
@@ -0,0 +1,52 @@
+package com.baeldung.spring.cloud.stream.rabbit;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.baeldung.spring.cloud.stream.rabbit.processor.RestProcessor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.support.MessageBuilder;
+
+@RestController
+@SpringBootApplication
+@EnableBinding(RestProcessor.class)
+public class RabbitController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private RestProcessor processor;
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitController.class, args);
+    }
+    
+    @StreamListener(RestProcessor.INPUT)
+    @SendTo(RestProcessor.OUTPUT)
+    public Greeting doGreeting(String name) {
+    	Greeting greeting = new Greeting(counter.incrementAndGet(), String.format(template, name));
+    	return greeting;    	
+    }
+
+    @RequestMapping("/greeting")
+    public Greeting restGreeting(@RequestParam(value="name", defaultValue="World") String name) {
+    	Greeting greeting = doGreeting(name);
+    	processor.restOutput().send(message(greeting));
+    	return greeting;
+    }
+    
+    private static final <T> Message<T> message(T val) {
+        return MessageBuilder.withPayload(val)
+            .build();
+    }
+}
